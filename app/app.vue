7c9ab4079272584009ab28497d6f21628701a3b4
Kuriyona Li <Kuriyona@outlook.com>
2026-05-07 17:39:21 +0800

    删除界面切换动画效果

--- app/app.vue
+++ app/app.vue
@@ -6,28 +6,7 @@ import AppBar from './components/AppBar.vue';
   <div class="relative h-screen scroll-hidden">
     <AppBar class="z-100 sticky top-0 backdrop-blur-lg" />
     <main>
-      <Transition name="page">
-        <NuxtPage id="main" class="z-0" />
-      </Transition>
+      <NuxtPage id="main" class="z-0" />
     </main>
   </div>
 </template>
-
-<style scoped>
-.page-enter-active,
-.page-leave-active {
-  transition:
-    opacity 0.5s ease,
-    transform 0.5s ease;
-}
-
-.page-enter-from {
-  opacity: 0;
-  transform: translateY(20px);
-}
-
-.page-leave-to {
-  opacity: 0;
-  transform: translateY(20px);
-}
-</style>
