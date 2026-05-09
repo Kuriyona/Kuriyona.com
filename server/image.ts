f17d1ea64be3964d7f46338ec48e3cf18aae3a37
Kuriyona Li <Kuriyona@outlook.com>
2026-05-10 02:08:44 +0800

    使用 Bun S3 API 替换 AWS SDK

--- server/image.ts
+++ server/image.ts
@@ -1,28 +1,21 @@
 import Elysia from 'elysia';
 import dayjs from 'dayjs';
 
-// s3
-import { S3Client, ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client } from 'bun';
 
 const s3 = new S3Client({
-  region: 'us-east-1',
   endpoint: process.env.ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID!,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
-  },
+  accessKeyId: process.env.ACCESS_KEY_ID!,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+  bucket: process.env.BUCKET_NAME!,
 });
 
 const app = new Elysia({ prefix: '/image' })
   .get('/list', async ({ params }) => {
-    const list = await s3.send(
-      new ListObjectsCommand({
-        Bucket: process.env.BUCKET_NAME,
-        Prefix: 'image',
-      }),
-    );
-    return list.Contents;
+    const list = await s3.list({
+      prefix: 'image',
+    });
+    return list.contents;
   })
   .get('/upload-signed-url', async ({ query: { filename, mime, auth }, set }) => {
     if (auth !== process.env.AUTH_KEY) {
@@ -30,13 +23,12 @@ const app = new Elysia({ prefix: '/image' })
       return null;
     }
     const key = `image/${dayjs().format('YYYY-MM')}/${dayjs().format('HH-mm-ss')}-${filename}`;
-    const command = new PutObjectCommand({
-      Bucket: process.env.BUCKET_NAME,
-      Key: key,
-      ContentType: mime,
+    const url = s3.presign(key, {
+      type: mime,
+      method: 'PUT',
     });
     return {
-      url: await getSignedUrl(s3, command),
+      url,
       key,
     };
   });
