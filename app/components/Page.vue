b7f0251bdb38ddae49311abb65823778eb34089a
Kuriyona Li <Kuriyona@outlook.com>
2026-05-16 03:04:17 +0800

    对友链页应用新设计

--- app/components/Page.vue
+++ app/components/Page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center h-full">
+  <div class="flex justify-center h-full mt-[68px]">
     <div class="w-160 my-20 p-4 flex flex-col gap-4">
       <slot />
       <var-divider :description="$t('global.about-site')" />
--- app/pages/links.vue
+++ app/pages/links.vue
@@ -1,23 +1,16 @@
 <script setup lang="ts">
-import { useI18n } from '~/scripts/i18n';
 import Config from '../config.json';
 </script>
 
 <template>
-  <div class="font-mono flex justify-center h-full">
-    <main class="w-160 my-20 p-4 flex flex-col gap-4">
-      <h1 class="text-2xl">
-        {{ $t('best_friends') }}
-      </h1>
-      <a :href="link.url" target="_blank" v-for="link in Config.links.main">
-        <CardLink :to="link.url" :text="link.title" :img="link.avatar" />
-      </a>
-      <h1 class="text-2xl mt-10">
-        {{ $t('other_friends') }}
-      </h1>
-      <a :href="link.url" target="_blank" v-for="link in Config.links.others">
-        <CardLink :to="link.url" :text="link.title" :img="link.avatar" :new="true" />
-      </a>
-    </main>
-  </div>
+  <Page>
+    <var-divider :description="$t('best_friends')" />
+    <a :href="link.url" target="_blank" v-for="link in Config.links.main">
+      <CardLink :to="link.url" :text="link.title" :img="link.avatar" />
+    </a>
+    <var-divider :description="$t('other_friends')" />
+    <a :href="link.url" target="_blank" v-for="link in Config.links.others">
+      <CardLink :to="link.url" :text="link.title" :img="link.avatar" :new="true" />
+    </a>
+  </Page>
 </template>
