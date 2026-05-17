a85a03849ed756f4b012e3de659db22b1896082b
Kuriyona Li <Kuriyona@outlook.com>
2026-05-17 22:49:50 +0800

    添加友链提示

--- app/pages/links.vue
+++ app/pages/links.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import Config from '../config.json';
+import { codeToHtml } from 'shiki';
 useSeoMeta({ title: $t('about.links') });
+const MY = ref(`const myLink = new Link()
+myLink.name = "Kuriyona"
+myLink.url = "https://kuriyona.com"
+myLink.desc = "${$t('about.description')}"
+myLink.avatar = "https://r2.kuriyona.com/img/avatar/Avatar_256.png"
+youLinks.push(myLink)
+`);
+onMounted(async () => {
+  MY.value = await codeToHtml(MY.value, { lang: 'ts', theme: 'github-dark' });
+});
 </script>
 
 <template>
@@ -13,5 +24,9 @@ useSeoMeta({ title: $t('about.links') });
     <a :href="link.url" target="_blank" v-for="link in Config.links.others">
       <CardLink :to="link.url" :text="link.title" :img="link.avatar" :new="true" />
     </a>
+    <var-divider :description="$t('links.add-me')" />
+    <Card>
+      <div v-html="MY" class="*:bg-transparent! *:text-wrap"></div>
+    </Card>
   </Page>
 </template>
