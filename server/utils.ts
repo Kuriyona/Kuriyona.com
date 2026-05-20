6640b19573cac4de5f96db496170b740f2b34bab
Kuriyona Li <Kuriyona@outlook.com>
2026-05-21 02:54:00 +0800

    PlusPlus 添加日限额

--- server/utils.ts
+++ server/utils.ts
@@ -1,6 +1,27 @@
+import dayjs from 'dayjs';
 import ky from 'ky';
 const apiKey = process.env.PUSHPLUS_API_KEY;
+const DAY_LIMIT = 100;
+
+let date = dayjs().format('YYYY-MM-DD');
+let count = 0;
+
+export const checkLimit = () => {
+  if (date !== dayjs().format('YYYY-MM-DD')) {
+    date = dayjs().format('YYYY-MM-DD');
+    count = 0;
+  }
+  count++;
+  return count <= DAY_LIMIT;
+};
+
 export const push = async (message: { title: string; content: string }) => {
+  if (!checkLimit()) {
+    return {
+      code: 429,
+      message: '请求频率过快',
+    };
+  }
   return await ky
     .post(`https://www.pushplus.plus/send`, {
       headers: {
