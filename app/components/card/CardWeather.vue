4f2a25dbc1865a498f8dde24c833cc46c43e9d71
Kuriyona Li <Kuriyona@outlook.com>
2026-05-23 16:43:54 +0800

    完成关于页所在地板块

--- app/components/card/CardWeather.vue
+++ app/components/card/CardWeather.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { Weather } from './types';
 
-const weather = ref<any>(undefined);
+const weather = ref<Weather>();
 
 onMounted(() => {
   fetchApi.get('/weather').then(async (res) => {
@@ -11,32 +11,46 @@ onMounted(() => {
 </script>
 
 <template>
-  <KTable v-if="weather">
-    <tbody class="bg-transparent">
-      <tr>
-        <th>天气</th>
-        <td>{{ weather.now.text }}</td>
-      </tr>
-      <tr>
-        <th>温度</th>
-        <td>{{ weather.now.temp }}℃</td>
-      </tr>
-      <tr>
-        <th>湿度</th>
-        <td>{{ weather.now.humidity }}%</td>
-      </tr>
-      <tr>
-        <th>风速</th>
-        <td>{{ weather.now.windSpeed }} m/s</td>
-      </tr>
-      <tr>
-        <th>风向</th>
-        <td>{{ weather.now.windDir }}</td>
-      </tr>
-      <tr>
-        <th>云量</th>
-        <td>{{ weather.now.cloud }}%</td>
-      </tr>
-    </tbody>
-  </KTable>
+  <div class="flex flex-col max-[600px]:flex-row max-[600px]:gap-4">
+    <KTable v-if="weather">
+      <tbody class="bg-transparent">
+        <tr>
+          <th>天气</th>
+          <td>{{ weather.now.text }}</td>
+        </tr>
+        <tr>
+          <th>温度</th>
+          <td>{{ weather.now.temp }}℃</td>
+        </tr>
+        <tr>
+          <th>体感</th>
+          <td>{{ weather.now.feelsLike }}℃</td>
+        </tr>
+        <tr>
+          <th>湿度</th>
+          <td>{{ weather.now.humidity }}%</td>
+        </tr>
+      </tbody>
+    </KTable>
+    <KTable v-if="weather">
+      <tbody class="bg-transparent">
+        <tr>
+          <th>风速</th>
+          <td>{{ weather.now.windSpeed }} m/s</td>
+        </tr>
+        <tr>
+          <th>风向</th>
+          <td>{{ weather.now.windDir }}</td>
+        </tr>
+        <tr>
+          <th>云量</th>
+          <td>{{ weather.now.cloud }}%</td>
+        </tr>
+        <tr>
+          <th>能见度</th>
+          <td>{{ weather.now.vis }} km</td>
+        </tr>
+      </tbody>
+    </KTable>
+  </div>
 </template>
--- app/pages/about/index.vue
+++ app/pages/about/index.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import Config from '@/config.json';
-import dayjs from 'dayjs';
-
-useSeoMeta({ title: $t('about.about-me') });
+const config = useAppConfig();
 </script>
 
 <template>
-  <AppPage class="gap-6">
-    <IAm />
+  <AppPage>
+    <div class="py-4 flex flex-col items-center justify-center gap-4 text-center px-10 mb-10">
+      <h1 class="text-2xl font-bold">
+        <span>你好，欢迎来到</span>
+        <br />
+        <span>Kuriyona's Space</span>
+      </h1>
+    </div>
     <div class="flex justify-center gap-4 max-[400px]:flex-col">
       <KCard :title="$t('about.about-me')" class="flex-1">
         <p>{{ $t('about.hello-i-am-kuriyona') }}</p>
@@ -15,71 +18,30 @@ useSeoMeta({ title: $t('about.about-me') });
         <br />
         <p class="font-bold">{{ $t('about.description') }}</p>
       </KCard>
+      <CardInfo />
     </div>
-    <var-divider :description="$t('about.about-me')" />
-    <ClientOnly>
-      <p>
-        {{
-          $t('about.days_on_earth', [
-            dayjs().diff('2008/6/28', 'year'),
-            dayjs().diff('2008/6/28', 'month'),
-            dayjs().diff('2008/6/28', 'day'),
-          ])
-        }}
-      </p>
-      <KWeather />
-    </ClientOnly>
-    <KCardLink to="/blog" :text="$t('blog.title')" />
-    <KCardLink
-      to="/blog/about-name"
-      :text="`${$t('global.read_more')} | 「${$t('about.about-my-name')}」`" />
-    <KCardLink to="/about/as-mtf">
+    <KCardLink to="/about-mtf">
       <template #content>
-        <span class="trans-text">
-          {{ $t('global.read_more') }} | 「{{ $t('about.mtf.as-mtf') }}」
-        </span>
+        <p class="trans-text">{{ $t('about.mtf.as-mtf') }}</p>
       </template>
     </KCardLink>
-    <var-divider :description="$t('about.skills')" />
-    <div class="flex gap-2 flex-wrap justify-center">
-      <img
-        v-for="item in Config.tech_stack"
-        :key="item.name"
-        :alt="item.name"
-        :src="`https://img.shields.io/badge/${encodeURIComponent(item.name)}-black?style=for-the-badge&logo=${item.icon}`" />
-    </div>
-    <div class="flex gap-2 flex-wrap justify-center">
-      <img
-        v-for="item in Config.languages"
-        :key="item.name"
-        :alt="$t(item.name)"
-        :src="`https://img.shields.io/badge/${item.icon}-${$t(item.name)}-black?style=for-the-badge`" />
-    </div>
-    <var-divider description="CONTACT" />
-    <div class="flex flex-wrap gap-2 justify-center">
-      <KCardLink
-        v-for="link in Config.contact"
-        :key="link.name"
-        :to="link.link"
-        :text="link.value"
-        :new="true">
-        <div class="flex items-center gap-4">
-          <span v-if="link.mdIcon" class="material-symbols-outlined"> mail </span>
+    <div class="flex justify-center gap-2 max-[600px]:flex-col">
+      <KCard :title="'未晞所在地'" class="flex-1">
+        <div class="relative max-h-40 h-full w-full">
+          <p class="absolute bottom-1 left-1 text-black/80 text-sm backdrop-blur-md p-1 rounded-md">
+            浙江省，杭州市，临安区
+          </p>
           <img
-            v-if="link.icon"
-            class="w-4 h-4"
-            :src="`https://cdn.simpleicons.org/${link.icon}/white`" />
-          <span>{{ link.i18nKey ? $t(link.i18nKey) : link.name }}</span>
+            class="h-full w-full object-cover max-[400px]:hidden"
+            src="https://r2.kuriyona.com/static/image/site/location-map-600.png" />
+          <img
+            class="h-full w-full object-cover min-[400px]:hidden"
+            src="https://r2.kuriyona.com/static/image/site/location-map-400.png" />
         </div>
-      </KCardLink>
+      </KCard>
+      <KCard :title="'未晞所在地天气'">
+        <CardWeather />
+      </KCard>
     </div>
-    <var-divider :description="$t('about.links')" />
-    <KCardLink
-      v-for="link in Config.links.main"
-      :to="link.url"
-      :text="link.title"
-      :img="link.avatar"
-      :new="true" />
-    <KCardLink to="/links" :text="$t('more')" />
   </AppPage>
 </template>
