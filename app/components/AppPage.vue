d16a7a75447492a8f6ca9b73b9247d81821b877f
Kuriyona Li <Kuriyona@outlook.com>
2026-05-24 03:25:12 +0800

    修复响应式布局错误

--- app/components/AppPage.vue
+++ app/components/AppPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center min-h-[calc(100vh)]">
-    <div class="w-200 my-20 p-4 flex flex-col gap-4">
+    <div class="w-200 max-w-full my-20 p-4 flex flex-col gap-4">
       <slot />
       <div class="flex-1"></div>
       <AppFooter />
