7439012390026112e0155b0955987876b3391ba0
Kuriyona Li <Kuriyona@outlook.com>
2026-05-27 21:02:15 +0800

    在标题栏添加头像

--- app/components/AppBar.vue
+++ app/components/AppBar.vue
@@ -9,10 +9,13 @@ const popup = ref(false);
 <template>
   <div class="sticky top-0 flex justify-center backdrop-brightness-75 backdrop-blur-3xl">
     <div class="w-200 px-4">
-      <div class="font-mono py-2 px-3 flex justify-between items-center rounded-xl">
-        <NuxtLinkLocale to="/">
-          <span class="hover:underline">Kuriyona's Space</span>
-        </NuxtLinkLocale>
+      <div class="relative font-mono h-12 py-2 px-3 flex justify-between items-center rounded-xl">
+        <div class="h-full flex items-center gap-4">
+          <img class="h-3/4 rounded-md" src="https://r2.kuriyona.com/img/avatar/Avatar_256.png" />
+          <NuxtLinkLocale to="/about">
+            <span class="hover:underline">Kuriyona's Space</span>
+          </NuxtLinkLocale>
+        </div>
         <div id="actions" class="flex gap-2">
           <VarButton round text @click="popup = true">
             <span class="material-symbols-outlined"> chat </span>
