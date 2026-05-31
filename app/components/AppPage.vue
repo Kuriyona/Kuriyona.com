c8efbe74ca068464f625f7ecda752307a91b301f
Kuriyona Li <Kuriyona@outlook.com>
2026-05-31 19:02:18 +0800

    Update AppPage.vue

--- app/components/AppPage.vue
+++ app/components/AppPage.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(ScrollTrigger);
 const pageRef = useTemplateRef<HTMLDivElement>('pageRef');
 
 onMounted(() => {
