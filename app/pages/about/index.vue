38f393d0962b7ec9249a4358b7026847b71f56d0
Kuriyona <Kuriyona@outlook.com>
2026-06-06 20:45:33 +0800

    更新联系信息排版

--- app/pages/about/index.vue
+++ app/pages/about/index.vue
@@ -91,14 +91,18 @@ const config = useAppConfig();
             :key="link.name"
             :to="!link.qrOnly ? link.link : undefined"
             :text="link.value"
-            :new="true">
-            <div class="flex items-center gap-4">
+            :new="true"
+            class="">
+            <div class="flex items-center gap-2">
               <span v-if="link.mdIcon" class="material-symbols-outlined"> mail </span>
               <img
                 v-if="link.icon"
                 class="w-4 h-4"
                 :src="`https://cdn.simpleicons.org/${link.icon}/white`" />
-              <span>{{ link.i18nKey ? $t(link.i18nKey) : link.name }}</span>
+              <div class="flex flex-col items-start">
+                <span>{{ link.i18nKey ? $t(link.i18nKey) : link.name }}</span>
+                <span v-if="link.value" class="text-xs text-white/50">{{ link.value }}</span>
+              </div>
             </div>
           </KCardLink>
           <template #content>
