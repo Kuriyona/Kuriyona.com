d60b1949300a643298b2f467a05e655999f41ee6
Kuriyona <Kuriyona@outlook.com>
2026-06-08 19:37:22 +0800

    为卡片添加边框

--- app/components/KCard.vue
+++ app/components/KCard.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 
 <template>
   <div
-    class="p-4 backdrop-brightness-50 backdrop-blur-sm rounded-xl text-white/80 text-sm"
+    class="p-4 backdrop-brightness-50 backdrop-blur-sm rounded-xl text-white/80 border border-white/5 text-sm"
     :class="{ 'shadow-lg shadow-white/3': $attrs.level !== undefined }">
     <h2 v-if="props.title" class="text-lg font-bold mb-4">{{ props.title }}</h2>
     <slot />
