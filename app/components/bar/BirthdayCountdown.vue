5950dbcdefc2b0a9374a1aa3c3c5800947af0323
Kuriyona <Kuriyona@outlook.com>
2026-06-08 22:13:52 +0800

    添加 NekoTips

--- app/components/bar/BirthdayCountdown.vue
+++ app/components/bar/BirthdayCountdown.vue
@@ -21,6 +21,7 @@ const countdownDays = computed(() => {
 const show = computed(() => {
   return countdownDays.value.asDays() <= 30 && !closed.value;
 });
+const showTips = useLocalStorage('showNekoTips', true);
 </script>
 
 <template>
@@ -29,6 +30,14 @@ const show = computed(() => {
       {{ $t('global.birthdayCountdown', [age, countdownDays.format('D [d] H [h] m [m] s [s]')]) }}
     </p>
     <KButton round text size="mini" @click="closed = true">
+      <span class="material-symbols-outlined text-sm! leading-none"> close </span>
+    </KButton>
+  </div>
+  <div v-if="showTips" class="flex items-center justify-between gap-2 text-sm text-white/80">
+    <p>
+      {{ $t('neko.tips') }}
+    </p>
+    <KButton round text size="mini" @click="showTips = false">
       <span class="material-symbols-outlined text-sm! leading-none"> close </span>
     </KButton>
   </div>
