56a8817a129a1b18dac989ed35acf889535a8e40
Kuriyona <Kuriyona@outlook.com>
2026-06-09 21:57:06 +0800

    完善 MusicBar

--- app/components/MusicBar.vue
+++ app/components/MusicBar.vue
@@ -38,7 +38,7 @@ const url = computed(() => {
   }
   return `https://meting.20100907.xyz/api?server=netease&type=url&id=${song.value?.id}`;
 });
-const cover = computed(() => song.value?.album?.picUrl);
+const cover = computed(() => song.value?.album?.picUrl + '?param=64x64');
 const audio = useTemplateRef<HTMLAudioElement>('audio');
 const { playing, currentTime } = useMediaControls(audio);
 const currentLyric = computed(() => {
@@ -48,20 +48,51 @@ const currentLyric = computed(() => {
 </script>
 
 <template>
-  <div v-if="song" class="h-16 flex items-center justify-between gap-2">
+  <div
+    v-if="song"
+    id="music-bar"
+    class="h-10 hover:h-16 flex items-center gap-2 text-nowrap truncate">
     <audio ref="audio" :src="url"></audio>
-    <!-- <img :src="cover" class="h-3/4 rounded-md" /> -->
-    <div class="flex items-center gap-2">
-      <KButton round text @click="playing = !playing">
-        <span class="material-symbols-outlined text-sm!">
-          {{ playing ? 'pause' : 'play_arrow' }}
-        </span>
-      </KButton>
-      <p class="text-sm">{{ song?.name }}</p>
-    </div>
-    <div class="flex flex-col gap-0.5 text-right">
-      <p class="text-xs">{{ currentLyric?.text }}</p>
-      <p v-if="currentLyric?.translation" class="text-xs">{{ currentLyric?.translation }}</p>
+    <img :src="cover" class="h-full hidden" />
+    <div class="flex justify-between w-full">
+      <div id="main" class="flex items-center gap-1 min-w-0 h-fit">
+        <div class="flex items-center">
+          <KButton round text @click="playing = !playing">
+            <span class="material-symbols-outlined">
+              {{ playing ? 'pause' : 'play_arrow' }}
+            </span>
+          </KButton>
+          <KButton round text class="hover-show">
+            <span class="material-symbols-outlined"> skip_previous </span>
+          </KButton>
+          <KButton round text class="hover-show">
+            <span class="material-symbols-outlined"> skip_next </span>
+          </KButton>
+        </div>
+        <p class="text-sm truncate hidden sm:block">{{ song?.name }}</p>
+      </div>
+      <div class="flex flex-col justify-center text-right min-w-0">
+        <p class="text-xs truncate">{{ currentLyric?.text }}</p>
+        <p v-if="currentLyric?.translation" class="text-xs! truncate">
+          {{ currentLyric?.translation }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+@reference "tailwindcss";
+.material-symbols-outlined {
+  @apply text-sm! leading-none;
+}
+.hover-show {
+  @apply hidden;
+}
+#music-bar {
+  @apply hover:[&>img]:block;
+  @apply hover:[&_#main]:flex-col-reverse hover:[&_#main]:items-start;
+  @apply hover:[&_.hover-show]:flex;
+  @apply transition-all duration-300;
+}
+</style>
