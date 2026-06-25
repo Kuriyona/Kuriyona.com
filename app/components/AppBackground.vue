46351f9e0626128cbe6a9d23c797501a989cce63
Kuriyona <Kuriyona@outlook.com>
2026-06-25 23:32:31 +0800

    降低背景亮度

--- app/components/AppBackground.vue
+++ app/components/AppBackground.vue
@@ -3,7 +3,7 @@ const Background = 'https://r2.kuriyona.com/img/Background/Columbina.mp4';
 </script>
 
 <template>
-  <div class="fixed w-full h-full top-0 left-0 z-[-1] brightness-10 select-none">
+  <div class="fixed w-full h-full top-0 left-0 z-[-1] brightness-5 select-none">
     <video
       class="w-full h-full object-cover object-[27%]"
       :src="Background"
