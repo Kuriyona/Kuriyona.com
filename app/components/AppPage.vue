b1df9703fd27448e4369912288aea30e92243099
Kuriyona <Kuriyona@outlook.com>
2026-06-30 04:32:44 +0800

    提高页面顶部边距

--- app/components/AppPage.vue
+++ app/components/AppPage.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex justify-center min-h-[calc(100vh)]">
+  <div class="flex justify-center min-h-[calc(100vh)] mt-25">
     <div class="w-200 max-w-full my-20 p-4 flex flex-col gap-4" ref="pageRef">
       <slot />
       <div class="flex-1"></div>
