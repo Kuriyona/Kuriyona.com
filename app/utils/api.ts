5073063a7f57554a16ed07327b14eeff426452f7
Kuriyona <Kuriyona@outlook.com>
2026-06-29 22:23:59 +0800

    使用新的本地开发 api url

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -2,7 +2,9 @@ import ky from 'ky';
 import { useStorage } from '@vueuse/core';
 const key = useStorage('API_KEY', '');
 
-export const HOST = import.meta.env.DEV ? 'http://localhost:62802' : 'https://api.kuriyona.com';
+export const HOST = import.meta.env.DEV
+  ? 'http://api-kuriyona-com.localhost/'
+  : 'https://api.kuriyona.com';
 
 export const fetchApi = ky.create({
   prefix: HOST,
