04a8da645ef6995b86b7e4194b3f98df8dfd8812
Kuriyona <Kuriyona@outlook.com>
2026-06-29 23:37:39 +0800

    ask-box 初步支持展示公开的问题

--- app/pages/ask-box/index.vue
+++ app/pages/ask-box/index.vue
@@ -1,12 +1,25 @@
 <script lang="ts" setup>
 const mainStore = useMainStore();
 const showTurnstile = ref(false);
+
 const form = ref({
   name: '',
   showName: true,
   question: '',
   showIP: false,
   note: '',
+});
+
+const { data: questions } = useAsyncData('questions', async () => {
+  const res = await fetchApi.get('/ask-box').json<
+    {
+      name?: string;
+      ip?: string;
+      question: string;
+      answer: string;
+    }[]
+  >();
+  return res;
 });
 const submit = async () => {
   const res = await fetchApi.post('/ask-box', {
@@ -27,6 +40,7 @@ useSeoMeta({ title: '提问箱' });
   <AppPage>
     <h2 class="text-xl font-bold">向未晞酱提问</h2>
     <p>请在下方填写并提交你的问题，留下身份或者匿名都行喵。</p>
+    <p>第一版就先这样吧，后面再调整 UI。</p>
     <var-form class="my-4 flex flex-col gap-2">
       <var-input placeholder="昵称(可选)" v-model="form.name" variant="outlined" />
       <var-input placeholder="问题" textarea v-model="form.question" variant="outlined" />
@@ -44,5 +58,21 @@ useSeoMeta({ title: '提问箱' });
       <var-button @click="showTurnstile = true" block> {{ $t('global.start') }} </var-button>
     </KCard>
     <var-button type="primary" block @click="submit"> {{ $t('global.submit') }} </var-button>
+
+    <template v-if="questions?.length">
+      <var-divider class="my-8!" />
+      <h3 class="text-xl font-bold">已经被回答并公开的问题</h3>
+      <p>后续会转移到单独的问题页面，不会显示在提问页面中。</p>
+      <KCard v-if="questions.length > 0" v-for="(question, index) in questions" :key="index">
+        <div class="flex flex-col gap-2">
+          <p class="text-sm" v-if="question.name">
+            来自 <span class="font-bold">{{ question.name }}</span> 的提问
+          </p>
+          <p class="text-base">{{ question.question }}</p>
+          <hr />
+          <p class="text-base">{{ question.answer }}</p>
+        </div>
+      </KCard>
+    </template>
   </AppPage>
 </template>
