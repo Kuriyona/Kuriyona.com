64a041d5604aaf69044adf35ad62c41c03e9953a
Kuriyona <Kuriyona@outlook.com>
2026-07-01 01:21:48 +0800

    更新友链配置

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,3 +1,69 @@
 import Config from '../Kuriyona/config.json';
 
-export default defineAppConfig(Config);
+export default defineAppConfig({
+  ...Config,
+  links: {
+    main: [
+      {
+        url: 'https://space.bilibili.com/1337924642',
+        title: '鱼块糯 - 哔哩哔哩',
+        avatar: 'https://r2.kuriyona.com/img/xFurina/xFurina.jpg',
+      },
+      {
+        url: 'https://github.com/lfcypo',
+        title: 'lfcypo - GitHub',
+        avatar: 'https://avatars.githubusercontent.com/u/110813385',
+      },
+    ],
+    others: [
+      {
+        url: 'https://launchpadx.top',
+        title: '启动台の小窝',
+        desc: '用代码改变世界',
+        avatar: 'https://avatars.githubusercontent.com/u/67402215',
+      },
+      {
+        url: 'https://paimon.com.cn',
+        title: '蜜酱胡萝卜派蒙',
+        avatar: 'https://paimon.com.cn/assets/img/logo.png',
+      },
+      {
+        url: 'https://chiyu.it',
+        title: '池鱼小栈',
+        desc: '｜д•´)!!',
+        avatar: 'https://chiyu.it/images/logo/logo.webp',
+      },
+      {
+        url: 'https://blog.qrasa.cn',
+        title: 'Qrasa 的小站',
+        avatar:
+          'https://blog.qrasa.cn/wp-content/uploads/2025/11/cropped-1764501521-3ca59cafd35748c38efd5d624a25243a-e1765684431787.jpeg',
+      },
+      {
+        url: 'https://zkn.moe',
+        title: 'kave',
+        desc: '一个灵魂处于布朗运动中的学生',
+        avatar: 'https://zkn.moe/profile.jpeg',
+      },
+      {
+        url: 'https://yuan1182.org',
+        title: '大明朱元璋',
+        avatar: 'https://yuan1182.org/avatar.jpg',
+      },
+    ],
+    links: [
+      {
+        url: 'https://www.nekro.top',
+        title: 'Nekro’s SEKAI',
+        desc: '自留地、日常记录、经验分享。',
+        avatar: 'https://avatars.githubusercontent.com/u/90670998',
+      },
+      {
+        url: 'https://blog.krisyan.dev',
+        title: 'Kris Yan',
+        desc: '無限進步 @Schale夏萊',
+        avatar: 'https://r2.kuriyona.com/static/image/avatar/krisyan.jpg',
+      },
+    ],
+  },
+});
